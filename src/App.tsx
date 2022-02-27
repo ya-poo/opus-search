348caeae3f502674e8483491df5ccb57aa102436
Kazufumi Okuda <kazu.okuda0205@gmail.com>
2022-02-27 23:48:48 +0900

    VFC に修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { VFC } from 'react';
 import './App.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/material';
@@ -6,19 +6,17 @@ import ComposerWorks from './page/ComposerWorks';
 import NavigationBar from './component/NavigationBar';
 import Composers from './page/Composers';
 
-function App() {
-  return (
-    <div className="App">
-      <NavigationBar />
-      <Container maxWidth="xl">
-        <Routes>
-          <Route path="/composers" element={<Composers />} />
-          <Route path="/composers/:composerCode" element={<ComposerWorks />} />
-          <Route path="*" element={<Navigate to="composers" replace />} />
-        </Routes>
-      </Container>
-    </div>
-  );
-}
+const App: VFC = () => (
+  <div className="App">
+    <NavigationBar />
+    <Container maxWidth="xl">
+      <Routes>
+        <Route path="/composers" element={<Composers />} />
+        <Route path="/composers/:composerCode" element={<ComposerWorks />} />
+        <Route path="*" element={<Navigate to="composers" replace />} />
+      </Routes>
+    </Container>
+  </div>
+);
 
 export default App;
